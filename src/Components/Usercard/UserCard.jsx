502cb88bf6909af4bac601b0ce3635989eaa2208
bradleymatjie <bradleymatjie.job@gmail.com>
2023-07-24 21:41:43 +0200

    added smooth transition on load

--- src/Components/Usercard/UserCard.jsx
+++ src/Components/Usercard/UserCard.jsx
@@ -1,36 +1,53 @@
 import './UserCard.scss';
 
 import pp from '../../assets/profile.jpeg';
+import { useEffect, useState } from 'react';
+
 
 export const UserCard = () => {
+    const [loaded, setloaded] = useState(false);
+    const [rotate, setRotate] = useState(false);
+
+    useEffect(() => {
+        setloaded(true);
+    }, [])
+    
+
+    const RotateFunc = () => {
+        setRotate(prev => !prev)
+    }
     return (
-        <div className="user-profile-card">
+        <div className={`user-profile-card ${loaded ? 'active': ''} ${rotate ? 'rotate': ''}`}>
             <img src={pp} alt="Profile" />
             <div>
                 <div className='user-profile-card_info'>
-                    <h2>Bradley Matjie</h2>
-                    <p>Software Engineer</p>
-                    <h3>Passionate Software Engineer with expertise in JavaScript+TypeScript, React, Java and Node.js</h3>
+                    <h2 className={`${rotate ? 'rotate': ''}`}>{rotate ? 'Lets Connect...':'Bradley Matjie'}</h2>
+                    <p className={`${rotate ? 'rotate': ''}`}>{rotate ? '':'Software Engineer'}</p>
+                    <h3 className={`${rotate ? 'rotate': ''}`}>{rotate ? 'Follow Me In Any Of These Social Media Accounts':'Passionate Software Engineer with expertise in JavaScript+TypeScript, React, Java and Node.js'}</h3>
                 </div>
             
                 <div className="stats">
-                    <div>
+                    {rotate ? <p className={`${rotate ? 'rotate': ''}`}><a href='https://github.com/bradleymatjie'>Github</a></p>:<div>
                         <p>19000</p>
                         <p>Followers</p>
-                    </div>
-                    <div>
+                    </div>}
+
+                    {rotate ? <p className={`${rotate ? 'rotate': ''}`}><a href='https://twitter.com/BradleyMatjie'>Twitter</a></p>:<div>
                         <p>203</p>
                         <p>Following</p>
-                    </div>
-                    <div>
+                    </div>}
+
+                    {rotate ? <p className={`${rotate ? 'rotate': ''}`}><a href='https://www.linkedin.com/in/bradleymatjie/'>LinkedIn</a></p>:<div>
                         <p>107</p>
                         <p>Projects</p>
-                    </div>
+                    </div>}
                 </div>
 
                 <div className="buttons">
                     <button>Follow</button>
-                    <button>View Profile</button>
+                    <button onClick={RotateFunc} className={`${rotate ? 'rotate': ''}`}>
+                        {rotate ? 'Back': 'View Profile'}
+                    </button>
                 </div>
             </div>
         </div>
